1cc033d6ab302e500a62e16adfa09aa4422926c6
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-08-07 15:59:23 +0530

    import java.util.Scanner; public class tri{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double c=3.14*a*a;         System.out.printf("%.2f",c);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class tri{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double c=3.14*a*a;
+        System.out.printf("%.2f",c);
+    }
+}
