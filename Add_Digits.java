58f451de86ded55bbe76f3c3edb8a1f2072c879f
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-08-07 16:02:55 +0530

    import java.util.Scanner; public class tri{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int c=a%9;         System.out.printf("%d",c);     } }

---
+++ Add_Digits.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class tri{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int c=a%9;
+        System.out.printf("%d",c);
+    }
+}
