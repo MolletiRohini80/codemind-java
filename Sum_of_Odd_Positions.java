90bf7513443788f94506914557876e56dfe4491b
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-08-19 12:05:27 +0530

    import java.util.Scanner; public class u{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         int s=0;         for(int i=0;i<n;i++){             a[i]=sc.nextInt();             if(i%2!=0){                 s+=a[i];             }         }         System.out.printf("%d",s);     } }

---
+++ Sum_of_Odd_Positions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class u{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            if(i%2!=0){
+                s+=a[i];
+            }
+        }
+        System.out.printf("%d",s);
+    }
+}
