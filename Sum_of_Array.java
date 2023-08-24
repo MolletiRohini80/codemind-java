2113c6898edd917223eea7e525c580dd3325beba
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-08-24 22:00:34 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int s=0,r=0;         for(int i=0;i<n;i++)         {             s+=a[i];         }         System.out.printf("%d",s);     } }

--- Sum_of_Array.java
+++ Sum_of_Array.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
-public class tt{
-    public static void main(String[] args){
+public class s{
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int intArr[]=new int[n];
-        int s=0;
-        for(int i=0;i<n;i++){
-            intArr[i]=sc.nextInt();
-            s+=intArr[i];
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int s=0,r=0;
+        for(int i=0;i<n;i++)
+        {
+            s+=a[i];
         }
         System.out.printf("%d",s);
     }
