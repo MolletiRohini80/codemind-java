6bb1d39ae4545811a2e74daf2c2cfa46a7bf4a2a
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-08-28 21:44:49 +0530

    import java.util.Scanner; public class tt{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int d=b*c;         if(a%d==0){             System.out.printf("%d",a/d);         }         else{             System.out.printf("%d",(a/d)+1);         }     } }

---
+++ Bags_and_Candies.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class tt{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=b*c;
+        if(a%d==0){
+            System.out.printf("%d",a/d);
+        }
+        else{
+            System.out.printf("%d",(a/d)+1);
+        }
+    }
+}
