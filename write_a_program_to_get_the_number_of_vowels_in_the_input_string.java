3df4b8f1ab46616fa4cf6155ebb6dc5c83d0c3d9
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-09-06 12:17:53 +0530

    import java.util.Scanner; public class tt{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String a="aeiouAEIUO";         int c=0;         for(int i=0;i<s.length();i++){             char ch=s.charAt(i);             if(a.contains(""+ch)){                 c+=1;             }         }         System.out.printf("%d",c);     } }

--- write_a_program_to_get_the_number_of_vowels_in_the_input_string.java
+++ write_a_program_to_get_the_number_of_vowels_in_the_input_string.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class tt{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
+        String s=sc.nextLine();
+        String a="aeiouAEIUO";
         int c=0;
-        for(int i=0;i<str.length();i++){
-            int ch=str.charAt(i);
-            if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'|| ch=='A'|| ch=='E'|| ch=='I'|| ch=='O'|| ch=='U'){
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(a.contains(""+ch)){
                 c+=1;
             }
         }
