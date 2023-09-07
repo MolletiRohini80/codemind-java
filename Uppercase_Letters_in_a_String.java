0b9a924a8ce1c5d8086743ecec004f6f6fbaf097
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-09-07 19:05:56 +0530

    import java.util.Scanner; public class tt{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int c=0;         for(int i=0;i<s.length();i++){             char a=s.charAt(i);             if(Character.isUpperCase(a)){                 c+=1;             }         }         System.out.printf("%d",c);     } }

---
+++ Uppercase_Letters_in_a_String.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class tt{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            if(Character.isUpperCase(a)){
+                c+=1;
+            }
+        }
+        System.out.printf("%d",c);
+    }
+}
