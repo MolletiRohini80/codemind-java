0c51365ba5de345e9a0cac1ed5e0fa08f856ad27
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-09-09 12:39:38 +0530

    import java.util.Scanner; public class tt{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int c=0;         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int maxx=0,max=0;         for(int i=0;i<n;i++)         {             c=0;             for(int j=0;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }             }             if(maxx==c)             {                 maxx=c;                if(a[i]<max)                {                    max=a[i];                }             }             else if(c>maxx)             {                 maxx=c;                 max=a[i];             }         }         System.out.println(max);     } }

---
+++ Most_Frequent.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class tt{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int maxx=0,max=0;
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            if(maxx==c)
+            {
+                maxx=c;
+               if(a[i]<max)
+               {
+                   max=a[i];
+               }
+            }
+            else if(c>maxx)
+            {
+                maxx=c;
+                max=a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
